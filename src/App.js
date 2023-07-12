e627e0f894e209b11d085dfcd366a962d3997358
Israr ahmad <119894610+Israr-Ahmad-01@users.noreply.github.com>
2023-07-12 12:12:12 +0500

    added date component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import "./components/Expenses"
-import './App.css';
+import "./components/Expenses";
+import "./App.css";
 import { Expenses } from "./components/Expenses";
 
-function App() {
+const App = () => {
+  const expenses = [
+    { title: "car Insurance", amount: 294.5, date: new Date(2021, 2, 28) },
+    { title: "Toilet Paper", amount: 94.5, date: new Date(2022, 5, 20) },
+    { title: "Books", amount: 24.5, date: new Date(2023, 4, 24) },
+    
+  ];
+
   return (
     <div>
-      <h2 className="text-warning">Expenses Calculator</h2>
-      <Expenses></Expenses>
+      <h1>Expenses Calculator</h1>
+      <Expenses title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></Expenses>
+      <Expenses title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></Expenses>
+      <Expenses title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></Expenses>
+
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,15 +2,14 @@ import React from "react";
 import "./Expenses.css";
 import Card from './UI/Card.js';
 
-export const Expenses = () => {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+export const Expenses = (props) => {
+
   return (
     <Card className="expenses">
-      <h4>{expenseDate.toISOString()}</h4>
-      <h5>{expenseTitle}</h5>
-      <h5>${expenseAmount}</h5>
+      {/* date component is not working so i leave it for future*/}
+      <h4>{props.date.toISOString()}</h4>
+      <h5>{props.title}</h5>
+      <h5>${props.amount}</h5>
       <button className="update_button">update!</button>
     </Card>
   );
